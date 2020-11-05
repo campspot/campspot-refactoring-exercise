d57e77dd46a6a29c739341eac1e8c9ef0aea9901
CaseyPeters <casey.peters@campspot.com>
2020-11-05 16:45:30 -0700

    gave it the good old college try.

--- spec/name-parser.spec.ts
+++ spec/name-parser.spec.ts
@@ -6,12 +6,12 @@ describe('parseFirstAndLastNameFromName', () => {
   describe('Simple Cases', () => {
     it('Should split a first and last name', () => {
         const expected = {firstName: "Zach", lastName: "Jones"};
-        const result = nameParser.getFirstAndLastName("Zach Jones");
+        const result = nameParser.getName("Zach Jones");
         expect(result).toEqual(expected);
     });
     it('duplicate a mononym', () => {
         const expected = {firstName: "Prince", lastName: "Prince"};
-        const result = nameParser.getFirstAndLastName("Prince");
+        const result = nameParser.getName("Prince");
         expect(result).toEqual(expected);
     });
   })
@@ -19,22 +19,22 @@ describe('parseFirstAndLastNameFromName', () => {
   describe('Suffixes', () => {
       it(`should add 'Jr.' to the end of the last name`, () => {
           const expected = {firstName: "Zach", lastName: "Jones Jr."};
-          const result = nameParser.getFirstAndLastName("Zach Jones Jr.");
+          const result = nameParser.getName("Zach Jones Jr.");
           expect(result).toEqual(expected);
       });
       it(`should add 'sr' to the end of the last name`, () => {
           const expected = {firstName: "Zach", lastName: "Jones sr."};
-          const result = nameParser.getFirstAndLastName("Zach Jones sr.");
+          const result = nameParser.getName("Zach Jones sr.");
           expect(result).toEqual(expected);
       });
       it(`should add 'Sr.' to the end of the last name`, () => {
           const expected = {firstName: "Zach", lastName: "Jones Sr."};
-          const result = nameParser.getFirstAndLastName("Zach Jones Sr.");
+          const result = nameParser.getName("Zach Jones Sr.");
           expect(result).toEqual(expected);
       });
       it(`should add 'IV' to the end of the last name`, () => {
           const expected = {firstName: "Zach", lastName: "Jones IV"};
-          const result = nameParser.getFirstAndLastName("Zach Jones IV");
+          const result = nameParser.getName("Zach Jones IV");
           expect(result).toEqual(expected);
       });
   });
@@ -42,43 +42,51 @@ describe('parseFirstAndLastNameFromName', () => {
   describe('Compound Names', () => {
       it(`should translate "The artist formerly known as Prince" to {first: "The artist formerly known as", last: "Prince"}`, () => {
           const expected = {firstName: "The artist formerly known as", lastName: "Prince"};
-          const result = nameParser.getFirstAndLastName("The artist formerly known as Prince");
+          const result = nameParser.getName("The artist formerly known as Prince");
           expect(result).toEqual(expected);
       });
       it(`should translate "The artist formerly known as Prince Jr." to {firstName: "The artist formerly known as", lastName: "Prince Jr."}`, () => {
           const expected = {firstName: "The artist formerly known as", lastName: "Prince Jr."};
-          const result = nameParser.getFirstAndLastName("The artist formerly known as Prince Jr.");
+          const result = nameParser.getName("The artist formerly known as Prince Jr.");
           expect(result).toEqual(expected);
       });
       it(`should translate "Sven, Sven, and Sven Jorgenson" to {firstName: "The artist formerly known as", lastName: "Prince}`, () => {
           const expected = {firstName: "Sven, Sven, and Sven", lastName: "Jorgenson"};
-          const result = nameParser.getFirstAndLastName("Sven, Sven, and Sven Jorgenson");
+          const result = nameParser.getName("Sven, Sven, and Sven Jorgenson");
           expect(result).toEqual(expected);
       });
       it(`should translate "Maria - Brings Her Own Vacuum" to {firstName: "Maria - Brings Her Own", lastName: "Vacuum"} (This is a real production case)`, () => {
           const expected = {firstName: "Maria - Brings Her Own", lastName: "Vacuum"};
-          const result = nameParser.getFirstAndLastName("Maria - Brings Her Own Vacuum");
+          const result = nameParser.getName("Maria - Brings Her Own Vacuum");
           expect(result).toEqual(expected);
       });
       it(`should translate "Angel del Mar" to {firstName: "Angel", lastName: "del Mar"}`, () => {
           const expected = {firstName: "Angel", lastName: "del Mar"};
-          const result = nameParser.getFirstAndLastName("Angel del Mar");
+          const result = nameParser.getName("Angel del Mar");
           expect(result).toEqual(expected);
       });
       it(`should translate "Carlos de la Cruz" to {firstName: "Carlos", lastName: "de la Cruz"}`, () => {
           const expected = {firstName: "Carlos", lastName: "de la Cruz"};
-          const result = nameParser.getFirstAndLastName("Carlos de la Cruz");
+          const result = nameParser.getName("Carlos de la Cruz");
           expect(result).toEqual(expected);
       });
       it(`should translate "Zach bin Steve" to {firstName: "Zach", lastName: "bin Steve"}`, () => {
           const expected = {firstName: "Zach", lastName: "bin Steve"};
-          const result = nameParser.getFirstAndLastName("Zach bin Steve");
+          const result = nameParser.getName("Zach bin Steve");
           expect(result).toEqual(expected);
       });
       it(`should translate "Santa St. Clause" to {firstName: "Santa", lastName: "St. Clause"}`, () => {
           const expected = {firstName: "Santa", lastName: "St. Clause"};
-          const result = nameParser.getFirstAndLastName("Santa St. Clause");
+          const result = nameParser.getName("Santa St. Clause");
           expect(result).toEqual(expected);
       });
   });
+
+  describe('Compound Names with Suffix', () => {
+        it(`should translate "Maria del Mar Jr." to {firstName: "Maria", lastName: "del Mar Jr."}`, () => {
+            const expected = {firstName: "Maria", lastName: "del Mar Jr."};
+            const result = nameParser.getName("Maria del Mar Jr.");
+            expect(result).toEqual(expected);
+        });
+    })
 });
--- src/name-parser.ts
+++ src/name-parser.ts
@@ -1,47 +1,44 @@
+import {Name} from "./name";
+
 export class NameParser {
 
     private readonly suffixRegex: RegExp = new RegExp(/^(((jr|sr)?(\.)*)|(i|ii|iii|iv|v|vi|vii|viii|ix|x))$/i);
     private readonly compoundNameRegex: RegExp = new RegExp(/(^(di|del|der|mc|le|la|dela|da|o|san|ten|ter|von|ver|vonder|vande|vander|te|bon|lo|pena|el|dos|du|st|ste|den|don|mac|bin|van|de)?(\.)*$)/i);
 
-    public getFirstAndLastName(name: string): firstAndLastName {
-        let firstName: string;
-        let lastName: string;
-        const partsOfName = name.trim().split(' ');
-        let suffix: string = "";
-        if (this.suffixRegex.test(partsOfName[partsOfName.length - 1])) {
-          suffix = partsOfName[partsOfName.length - 1];
-          partsOfName.pop();
-        }
-        firstName = partsOfName[0];
-        lastName = partsOfName[partsOfName.length - 1];
-        if (partsOfName.length > 2) {
-          partsOfName.pop();
-          let isCompoundLastName = false;
-          for (let i = 1; i < partsOfName.length; i++) {
-            if (this.compoundNameRegex.test(partsOfName[i])) {
-              isCompoundLastName = true;
-            }
-            if (isCompoundLastName) {
-              for (let y = partsOfName.length - 1; y >= i; y--) {
-                lastName = `${partsOfName[y]} ${lastName}`;
-              }
-              partsOfName.splice(i, partsOfName.length - i);
-              break;
-            }
-          }
-          firstName = partsOfName.join(' ');
-        }
-        if (suffix) {
-          lastName += ` ${suffix}`;
-        }
-        return {
-          firstName: firstName,
-          lastName: lastName
-        };
-      }
-}
+    public getName(fullName: string): Name {
+        let partsOfName = fullName.split(' ');
 
-export class firstAndLastName {
-    firstName!: String;
-    lastName!: String;
+        if (partsOfName.length === 1) {
+            return {
+                first: fullName,
+                last: fullName
+            }
+        } else if (partsOfName.length === 2) {
+            return {
+                first: partsOfName[0],
+                last: partsOfName[1]
+            }
+        } else {
+            let lastIndex = partsOfName.length - 1
+            let splitIndex = lastIndex;
+
+            //if we have a suffix move the split point to the index of the word before the suffix
+            if (this.suffixRegex.test(partsOfName[lastIndex])) {
+                splitIndex = lastIndex - 1;
+            }
+
+            //if we have a compound last name, then move the split point to the index of the first compound last name
+            for (let i = 0; i < partsOfName.length - 1; i++) {
+                if (this.compoundNameRegex.test(partsOfName[i])) {
+                    splitIndex = i;
+                    break;
+                }
+            }
+
+            return {
+                first: partsOfName.slice(0, splitIndex).join(' '),
+                last: partsOfName.slice(splitIndex, partsOfName.length).join(' ')
+            }
+        }
+    }
 }
